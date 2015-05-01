82fe9f6ec74e9797ae7dd72eca6d040c43714e30
jollaman999 <admin@jollaman999.com>
2015-05-01 11:02:44 +0900

    Add missing files

--- e975-vendor-blobs.mk
+++ e975-vendor-blobs.mk
@@ -157,7 +157,13 @@ PRODUCT_COPY_FILES += \
     vendor/lge/e975/proprietary/vendor/lib/egl/libGLESv2_adreno.so:system/vendor/lib/egl/libGLESv2_adreno.so \
     vendor/lge/e975/proprietary/vendor/lib/egl/libq3dtools_adreno.so:system/vendor/lib/egl/libq3dtools_adreno.so \
     vendor/lge/e975/proprietary/vendor/lib/libC2D2.so:system/vendor/lib/libC2D2.so \
-    vendor/lge/e975/proprietary/lib/liblgftmitem.so:system/lib/liblgftmitem.so \
-    vendor/lge/e975/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
+    vendor/lge/e975/proprietary/vendor/lib/libCB.so:system/vendor/lib/libCB.so \
+    vendor/lge/e975/proprietary/vendor/lib/libadreno_utils.so:system/vendor/lib/libadreno_utils.so \
+    vendor/lge/e975/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
+    vendor/lge/e975/proprietary/vendor/lib/libc2d30-a3xx.so:system/vendor/lib/libc2d30-a3xx.so \
+    vendor/lge/e975/proprietary/vendor/lib/libc2d30.so:system/vendor/lib/libc2d30.so \
+    vendor/lge/e975/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so \
+    vendor/lge/e975/proprietary/vendor/lib/libllvm-qcom.so:system/vendor/lib/libllvm-qcom.so \
     vendor/lge/e975/proprietary/vendor/lib/libsc-a3xx.so:system/vendor/lib/libsc-a3xx.so \
-    vendor/lge/e975/proprietary/vendor/lib/libgsl.so:system/vendor/lib/libgsl.so
+    vendor/lge/e975/proprietary/lib/liblgftmitem.so:system/lib/liblgftmitem.so \
+    vendor/lge/e975/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so
